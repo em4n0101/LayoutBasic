e905cf272b6833cfe606d43dbfe81567c1ce3a55
Alejandro Barranco <alexbar10@live.com.mx>
2024-01-07 03:06:16 -0600

    Add WelcomeScreen and SignInScreen using ConstraintLayout

--- app/src/main/java/com/alexbar/layoutbasic/ui/theme/Color.kt
+++ app/src/main/java/com/alexbar/layoutbasic/ui/theme/Color.kt
@@ -11,3 +11,5 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val BackgroundColor = Color(0xFFFBEAE4)
+val BackgroundColorGradientStart = Color(0xFFA52B3A)
+val BackgroundColorGradientEnd = Color(0xFF291835)
--- app/src/main/java/com/alexbar/layoutbasic/ui/theme/Type.kt
+++ app/src/main/java/com/alexbar/layoutbasic/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.alexbar.layoutbasic.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -14,12 +15,12 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.White,
+        fontSize = 28.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
@@ -30,5 +31,4 @@ val Typography = Typography(
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
